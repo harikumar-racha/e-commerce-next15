47cb7ec051da7556e804e2908b6b71d49ffe1616
harikumarracha <r.hari@cendyn.com>
2025-08-11 16:56:10 +0530

    created products page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 import Navigation from "./components/Navigation";
 import Footer from "./components/Footer";
+import Link from "next/link";
 
 export default function Homepage() {
   return (
     <div className="min-h-screen flex flex-col">
       <Navigation />
+      <main className="flex-grow">
+        <h1>Welcome to the homepage</h1>
+        <Link href="/products" className="font-weight-500 underline">Go to products page</Link>
+      </main>
       <Footer />
     </div>
   );
