7c14417aa8ca7bd90dcf50051e53c1ecbb6d9e1a
harikumarracha <r.hari@cendyn.com>
2025-09-03 18:29:32 +0530

    hero section completed

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Navigation = () => {
   return (
-    <nav className='fixed top-0 left-0 right-0 bg-[#003d5d] shadow-md py-4'>
+    <nav className='fixed top-0 left-0 right-0 bg-[#003d5d] shadow-md py-4 z-50'>
       <div className="max-w-7xl mx-auto px-4">
         <div className='flex justify-between items-center'>
           <Link href="/" className='flex items-center'>
