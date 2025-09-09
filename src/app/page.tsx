6e8809ea93a5ee78e7527f06c65f8e06ca11dc68
harikumarracha <r.hari@cendyn.com>
2025-09-09 17:30:31 +0530

    products listing page completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Navigation from "./components/Navigation";
 import Footer from "./components/Footer";
-import Link from "next/link";
+import Link from 'next/link';
 import Image from "next/image";
 
 export default function Homepage() {
